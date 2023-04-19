01b20a523f865f4b97ca65155984f70d4f24bf7f
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2023-04-18 21:00:15 -0500

    setting up profile backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,38 @@
-const {} = require('../models')
+const { Profile, User } = require('../models')
 
 const resolvers = {
+Query: {
+    profile: async (parent, { profileId }) => {
+        return Profile.findOne({ _id: profileId });
+      },
+    profiles: async () => {
+        return await Profile.find({})
+    }
+},
 
+Mutation: {
+    addProfile: async (parent, { username, age, sex, weight, height, goalWeight, dailyCalories }, context) => {
+        if (context.user) {
+        return await Profile.create({ username, age, sex, weight, height, goalWeight, dailyCalories });
+        }
+        throw new AuthenticationError('You need to be logged in!');
+      },
+      updateProfile: async (parent, { id, age, weight, height, goalWeight, dailyCalories }, context) => {
+        if (context.user) {
+        return await Profile.findOneAndUpdate(
+          { _id: id }, 
+          { age },
+          { weight },
+          { height },
+          { goalWeight },
+          { dailyCalories },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    }
 }
+
+};
 
 module.exports = resolvers;
--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -1,7 +1,27 @@
 const { gql } = require('apollo-server-express')
 
 const typeDef = gql`
+type Profile {
+    _id: ID!
+    username: String!
+    age: Int!
+    sex: String!
+    weight: Int! 
+    height: Int! 
+    goalWeight: Int!
+    dailyCalories: Int
+    savedRoutines: [Routine]
+}
 
+type Query {
+    profiles: [Profile]
+    profile(profileId: ID!): Profile
+}
+
+type Mutation{
+    addProfile( username: String!, age: Int!, sex: String!, weight: Int!, height: Int!, goalWeight: Int!, dailyCalories: Int): Profile
+    updateProfile(id: ID!, age: Int!, weight: Int!, height: Int!, goalWeight: Int!, dailyCalories: Int): Profile
+}
 `
 
 module.exports = typeDef;
