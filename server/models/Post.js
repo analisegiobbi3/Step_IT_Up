6a95cc9b913ccc09252ef65aeadad2cd08f4c6a2
Analise <analise.giobbi@gc.com>
2023-04-18 21:55:10 -0400

    backend work for blog

---
+++ server/models/Post.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+const postSchema = new Schema (
+    {
+        title: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 300,
+            trim: true,
+        },
+        text: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        author: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        comments: [
+            {
+                commentText: {
+                    type: String,
+                    require: true,
+                    minlength: 1,
+                    maxlength: 300,
+                },
+                commentAuthor: {
+                    type: String,
+                    required: true,
+                },
+                createdAt: {
+                    type: Date,
+                    default: Date.now,
+                    get: (timestamp) => dateFormat(timestamp),
+                },
+            },
+        ],
+    }
+)
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -1,6 +1,34 @@
 const { gql } = require('apollo-server-express')
 
 const typeDef = gql`
+    type Post {
+        _id: ID
+        title: String
+        text: String
+        author: String
+        createdAt: String
+        comments: [Comment]!
+    }
+
+    type Comment{
+        _id: ID
+        commentText: String
+        commentAuthor: String
+        createdAt: String
+    }
+
+    type Query {
+        posts: (username: String) [Post]!
+        post(postId: ID!): Post
+    }
+
+    type Mutation{
+        addPost(title: String!, text: String!): Post
+        addComment(postId: ID!, commentText: String!): Post
+        editPost(postId: ID!, title: String!, text: String!): Post
+        removePost(postId: ID!): Post
+        removeComment(postId: ID!, commentId: ID!): Post
+    }
 
 `
 
