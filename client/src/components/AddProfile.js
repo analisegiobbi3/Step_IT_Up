d3f4fe28dbe43745c0e1ecf9f0c504ed4a7eb44a
Savita Rankavat <manchandasavita3@gmail.com>
2023-04-29 12:48:02 -0500

    styling profile

--- client/src/components/AddProfile.js
+++ client/src/components/AddProfile.js
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
-import { Link, Form } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useMutation} from '@apollo/client';
-import { Box, FormControl, FormLabel, Input, FormHelperText, Button, Radio, RadioGroup, HStack } from '@chakra-ui/react';
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Button,
+  RadioGroup,
+  Radio,
+  HStack,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Select
+} from "@chakra-ui/react";
 
 import { ADD_PROFILE } from "../utils/mutations";
 
 import Auth from '../utils/auth';
+import '../styles/Profile.css';
 
 const AddProfile = () => {
   const [newAge, setNewAge] = useState(' ');
@@ -38,83 +58,163 @@ const AddProfile = () => {
     if (newSex === "Male") {
       const bmr =
         88.362 +
-        (13.397 * (newGoalWeight * 2.2) + 4.799 * newHeight - 5.677 * newAge);
+        (13.397 * (newGoalWeight/ 2.2) + (4.799 * newHeight) - (5.677 * newAge));
       setCalories(Math.round(bmr * activityLevel));
     } else {
       const bmr =
         447.593 +
-        (9.247 * (newGoalWeight * 2.2) + 3.098 * newHeight - 4.33 * newAge);
+        (9.247 * (newGoalWeight/ 2.2) + (3.098 * newHeight) - (4.33 * newAge));
       setCalories(Math.round(bmr * activityLevel));
     }
   };
+
+      const { isOpen } = useDisclosure({ defaultIsOpen: true });
+      const navigate = useNavigate();
+      const returnToHome = () => navigate("/");
+
+
     return (
       <div>
-        {Auth.loggedIn() ? (
-          <>
-            {/* <Box maxW='480px'> */}
-            <form onSubmit={handleFormSubmit}>
-              {/* <FormControl isRequired mb='20px'> */}
-              <label>Age</label>
-              <input
-                defaultValue={newAge}
-                className="form-input"
-                onChange={(e) => setNewAge(parseInt(e.target.value))}
-              />
-              <label>Height</label>
-              <input
-                defaultValue={newHeight}
-                className="form-input"
-                onChange={(e) => setNewHeight(parseInt(e.target.value))}
-              />
-              {/* </FormControl> */}
-              {/* <FormControl isRequired mb='20px'> */}
-              <label>Birth Sex</label>
-              <select value={newSex} onChange={(e) => setNewSex(e.target.value)}>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
-              <label>Weight</label>
-              <input
-                defaultValue={newWeight}
-                // value={me.goalWeight}
-                className="form-input"
-                onChange={(e) => setNewWeight(parseInt(e.target.value))}
-              />
-              <label>Goal Weight</label>
-              {/* <FormHelperText>If you are working on maintenance, you can enter your current weight here
-            to help us personalise your experience on this app.</FormHelperText> */}
-              <input
-                defaultValue={newGoalWeight}
-                className="form-input"
-                onChange={(e) => setNewGoalWeight(parseInt(e.target.value))}
-              />
-              {/* </FormControl> */}
-              <label>Activity Level</label>
-              <select
-                defaultValue={activityLevel}
-                onChange={(e) => setActivityLevel(parseInt(e.target.value))}
-              >
-                <option value="1.375">Sedentary</option>
-                <option value="1.55">Moderate</option>
-                <option value="1.9">High</option>
-              </select>
-              <button onClick={calculateCalories}>
-                Calculate your calorie intake according to your goal weight
-              </button>
-              <div>{showCalories ? <p>{calories}</p> : ""}</div>
-              <div className="button">
-                <button type="submit">save changes</button>
-              </div>
-              {error && <div className="danger">Something went wrong..</div>}
-            </form>
-            {/* </Box> */}
-          </>
-        ) : (
-          <p>
-            You need to be logged in to view your profile. Please{" "}
-            <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-          </p>
-        )}
+        <Box className="profile-modal">
+          <Modal isOpen={isOpen} onClose={returnToHome}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Are you ready to step it up?</ModalHeader>
+              <ModalCloseButton />
+              {Auth.loggedIn() ? (
+                <>
+                  {/* <Box maxW='480px'> */}
+                  <form onSubmit={handleFormSubmit}>
+                    <ModalBody>
+                      <FormControl isRequired mb="20px">
+                        <FormLabel>Age</FormLabel>
+                        <Input
+                          value={newAge}
+                          className="form-input"
+                          onChange={(e) => setNewAge(parseInt(e.target.value))}
+                        />
+                      </FormControl>
+                      <FormControl isRequired mb="20px">
+                        <FormLabel>Birth Sex</FormLabel>
+                        <Select
+                          value={newSex}
+                          onChange={(e) => setNewSex(e.target.value)}
+                        >
+                          <option value=' '>Please select an option</option>
+                          <option value="Male">Male</option>
+                          <option value="Female">Female</option>
+                        </Select>
+                      </FormControl>
+                      <FormControl isRequired mb="20px">
+                        <FormLabel>Height</FormLabel>
+                        <FormHelperText mb="20px">(cm)</FormHelperText>
+                        <Input
+                          value={newHeight}
+                          className="form-input"
+                          onChange={(e) =>
+                            setNewHeight(parseInt(e.target.value))
+                          }
+                        />
+                      </FormControl>
+                      <FormControl isRequired mb="20px">
+                        <FormLabel>Weight</FormLabel>
+                        <FormHelperText mb="20px">(lbs)</FormHelperText>
+                        <Input
+                          value={newWeight}
+                          // value={me.goalWeight}
+                          className="form-input"
+                          onChange={(e) =>
+                            setNewWeight(parseFloat(e.target.value))
+                          }
+                        />
+                      </FormControl>
+                      <FormControl isRequired mb="20px">
+                        <FormLabel>Goal Weight</FormLabel>
+                        <FormHelperText mb="20px">(lbs)</FormHelperText>
+                        <FormHelperText mb="20px">
+                          If you are working on maintenance, you can enter your
+                          current weight to help us personalise your experience
+                          on this app.
+                        </FormHelperText>
+                        <Input
+                          value={newGoalWeight}
+                          className="form-input"
+                          onChange={(e) =>
+                            setNewGoalWeight(parseInt(e.target.value))
+                          }
+                        />
+                      </FormControl>
+                      <FormControl isRequired mb="20px">
+                        <FormLabel>Activity Level</FormLabel>
+                        <Select
+                          defaultValue={activityLevel}
+                          onChange={(e) => setActivityLevel(parseInt(e.target.value))}
+                        >
+                          <option value=''>Please select an option</option>
+                          <option value="1.2">Sedentary</option>
+                          <option value="1.375">Moderate</option>
+                          <option value="1.55">High</option>
+                        </Select>
+                      </FormControl>
+                      <FormControl mb="20px">
+                        <Button
+                          type="button"
+                          colorScheme="facebook"
+                          onClick={calculateCalories}
+                          _hover={{
+                            bg: "var(--shade2)",
+                            color: "var(--shade6)",
+                          }}
+                        >
+                          calculate your calorie intake
+                        </Button>
+                        <div className="clories">
+                          {showCalories ? <p>{calories} calories/day</p> : ""}
+                        </div>
+                      </FormControl>
+                    </ModalBody>
+                    <ModalFooter>
+                      <div className="button">
+                        <Button
+                          colorScheme="facebook"
+                          mr={3}
+                          onClick={returnToHome}
+                          _hover={{
+                            bg: "var(--shade2)",
+                            color: "var(--shade6)",
+                          }}
+                        >
+                          close
+                        </Button>
+                        <Button
+                          bg="var(--shade1)"
+                          color="white"
+                          _hover={{
+                            bg: "var(--shade2)",
+                            color: "var(--shade6)",
+                          }}
+                          type="submit"
+                        >
+                          save
+                        </Button>
+                      </div>
+                      {error && (
+                        <div className="danger">Something went wrong..</div>
+                      )}
+                    </ModalFooter>
+                  </form>
+                  {/* </Box> */}
+                </>
+              ) : (
+                <p>
+                  You need to be logged in to view your profile. Please{" "}
+                  <Link to="/login">login</Link> or{" "}
+                  <Link to="/signup">signup.</Link>
+                </p>
+              )}
+            </ModalContent>
+          </Modal>
+        </Box>
       </div>
     );
 
